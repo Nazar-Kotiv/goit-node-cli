b68caab20e96230ac4b26d811b4d47284717ead8
Nazar-Kotiv <kotnazar20@gmail.com>
2024-04-17 17:17:57 +0200

    commit

--- src/contscts.js
+++ src/contscts.js
@@ -1,19 +1,55 @@
+import { error } from "console";
 import { promises as fs } from "fs";
 import path from "path";
 
 const contactsPath = path.join(__dirname, "contacts.json");
 
-fs.readdir(__dirname)
-  .then((files) => {
-    return Promise.all(
-      files.map(async (filename) => {
-        const stats = await fs.stat(filename);
-        return {
-          Name: filename,
-          Size: stats.size,
-          Date: stats.mtime,
-        };
-      })
-    );
-  })
-  .then((result) => console.table(result));
+async function listContacts() {
+  const data = await fs.readFile(contactsPath, { encoding: "utf-8" });
+  return JSON.parse(data);
+  // ...твій код. Повертає масив контактів.
+}
+listContacts().catch((error) => console.error(error));
+
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const contact = data.find((contact) => contact.id === contactId);
+  return contact || null;
+  // ...твій код. Повертає об'єкт контакту з таким id. Повертає null, якщо контакт з таким id не знайдений.
+}
+getContactById().catch((error) => console.error(error));
+
+async function removeContact(contactId) {
+  const data = await listContacts();
+  const deletContact = data.filter((iteam) => iteam.id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(deletContact, null, 2), {
+    encoding: "utf-8",
+  });
+
+  return { deletedContactId: contactId };
+
+  // ...твій код. Повертає об'єкт видаленого контакту. Повертає null, якщо контакт з таким id не знайдений.
+}
+removeContact().catch((error) => console.error(error));
+
+async function addContact(name, email, phone) {
+  const data = await listContacts();
+  const maxId = contacts.reduce(
+    (max, contact) => (contact.id > max ? contact.id : max),
+    0
+  );
+  const newContact = {
+    id: maxId + 1,
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2), {
+    encoding: "utf-8",
+  });
+  return newContact;
+  // ...твій код. Повертає об'єкт доданого контакту (з id).
+}
+
+addContact().catch((error) => console.error(error));
